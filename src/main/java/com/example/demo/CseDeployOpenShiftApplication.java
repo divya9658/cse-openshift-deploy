57e9706b1d9bb0bfdeb06ea753761cbdc5b6f1ef
divya9658 <23mh1a05m4@acoe.edu.in>
2026-03-27 23:12:54 +0530

    initial commit

---
+++ src/main/java/com/example/demo/CseDeployOpenShiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CseDeployOpenShiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CseDeployOpenShiftApplication.class, args);
+	}
+
+}
